93a3995be30ee12d7a122b74e4e26443ca55d625
KzyzOff <krzysztof.dybus@o2.pl>
2022-01-25 18:06:45 +0100

    Restructured directory structure of project

--- header.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef MINESWEEPER_HEADER_H
-#define MINESWEEPER_HEADER_H
-
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
-
-#endif
---
+++ src/header.h
@@ -0,0 +1,9 @@
+
+#ifndef MINESWEEPER_HEADER_H
+#define MINESWEEPER_HEADER_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+#endif
